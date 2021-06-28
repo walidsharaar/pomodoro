d696360e8bdfd7ea04ca93562491e58448ef7a0b
Sharaar <walidsharar@gmail.com>
2021-06-28 17:39:10 +0200

    count down added

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import math
 from tkinter import *
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -12,9 +13,17 @@ LONG_BREAK_MIN = 20
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+    count_down(5*60)
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def count_down(count):
+    count_min = math.floor(count/60)
+    count_sec = count%60
 
+    canvas.itemconfig(timer_text,text=f"{count_min}:{count_sec}")
+    if count>0:
+        window.after(1000,count_down,count=-1)
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
@@ -30,7 +39,7 @@ canvas.create_image(100,100,image=image)
 canvas.create_text(100,132,text="00:00", fill="white", font=(FONT_NAME,35,"bold"))
 canvas.grid(column=1,row=1)
 
-start_button=Button(text="Start",highlightthickness=0)
+start_button=Button(text="Start",highlightthickness=0,commend=start_timer)
 start_button.grid(column=0,row=2)
 
 reset_button=Button(text="Reset",highlightthickness=0)
