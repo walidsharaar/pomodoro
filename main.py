5e3f42dafcf14dd05717866d07e426e70c1b2a33
Sharaar <walidsharar@gmail.com>
2021-06-28 17:02:02 +0200

    Add image in the screen

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+from tkinter import *
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -16,3 +16,17 @@ LONG_BREAK_MIN = 20
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 # ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100,pady=50, bg=YELLOW)
+canvas = Canvas(width=200,height=225, bg=YELLOW,highlightthickness=0)
+image=PhotoImage(file="tomato.png")
+canvas.create_image(100,100,image=image)
+canvas.create_text(100,132,text="00:00", fill="white", font=(FONT_NAME,35,"bold"))
+canvas.pack()
+
+
+
+
+window.mainloop()
